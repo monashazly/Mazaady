00e70dd16fe620ade69f312b571e00007c6e510b
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-08-21 14:43:53 +0300

    responsive

--- components/Product.vue
+++ components/Product.vue
@@ -9,7 +9,7 @@ const active = ref(false)
 
 <template>
     <div class="tw-flex tw-justify-between" style="position: relative;">
-        <div class=" tw-relative tw-rounded-[19px] tw-overflow-hidden tw-w-[28%] tw-h-fit">
+        <div class=" tw-relative tw-rounded-[19px] tw-overflow-hidden tw-w-[18%] tw-max-h-[150px]">
             <NuxtImg :src="product.image" class="tw-w-full" />
             <div class=" tw-text-white tw-text-xs tw-absolute tw-w-[124px] tw-bottom-[-28px] tw-right-[-43px] tw-h-14 "
                 :class="product.sale ? 'tw-bg-secondary tw-py-2 tw-pl-5' : 'tw-bg-primary tw-py-2 tw-pl-2'"
@@ -17,7 +17,7 @@ const active = ref(false)
                 {{ product.sale ? 'Hot Sale' : 'Live auction' }}
             </div>
         </div>
-        <div class="tw-flex tw-justify-between  tw-w-[65%]">
+        <div class="tw-flex tw-justify-between  tw-w-[78%]">
             <div class="tw-flex tw-flex-col tw-gap-1">
                 <span>{{ product.name }}</span>
                 <h5> <span class=" tw-text-sm tw-text-zinc-400">Starting Price </span> {{ product.price }} EGP</h5>
