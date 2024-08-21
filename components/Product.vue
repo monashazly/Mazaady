33afb6061800d2712e0679779c2bc0183aa8eff0
Mona Shazly <monashazly@Monas-MacBook-Air.local>
2024-08-21 14:29:28 +0300

    hearrt icon in mobile

--- components/Product.vue
+++ components/Product.vue
@@ -8,22 +8,22 @@ const active = ref(false)
 </script>
 
 <template>
-    <div class="tw-flex tw-justify-between">
-        <div class=" tw-relative tw-rounded-[19px] tw-overflow-hidden tw-w-[25%] tw-h-fit">
-            <NuxtImg :src="product.image" class="tw-w-full"  />
+    <div class="tw-flex tw-justify-between" style="position: relative;">
+        <div class=" tw-relative tw-rounded-[19px] tw-overflow-hidden tw-w-[28%] tw-h-fit">
+            <NuxtImg :src="product.image" class="tw-w-full" />
             <div class=" tw-text-white tw-text-xs tw-absolute tw-w-[124px] tw-bottom-[-28px] tw-right-[-43px] tw-h-14 "
                 :class="product.sale ? 'tw-bg-secondary tw-py-2 tw-pl-5' : 'tw-bg-primary tw-py-2 tw-pl-2'"
                 style="border-radius: inherit;">
                 {{ product.sale ? 'Hot Sale' : 'Live auction' }}
             </div>
         </div>
-        <div class="tw-flex tw-justify-between  tw-w-[68%]">
+        <div class="tw-flex tw-justify-between  tw-w-[65%]">
             <div class="tw-flex tw-flex-col tw-gap-1">
                 <span>{{ product.name }}</span>
                 <h5> <span class=" tw-text-sm tw-text-zinc-400">Starting Price </span> {{ product.price }} EGP</h5>
                 <h5 class="tw-flex" :class="$vuetify.display.smAndDown ? ' tw-flex-col' : 'tw-justify-start tw-gap-2'">
                     <span class=" tw-text-sm tw-text-zinc-400">Lot starts in </span>
-                    <span class="tw-flex tw-flex-wrap tw-gap-2" >
+                    <span class="tw-flex tw-flex-wrap tw-gap-2">
                         <Chip class=" tw-text-xs">
                             <template #content>
                                 {{ product.days }} Days
@@ -43,7 +43,9 @@ const active = ref(false)
 
                 </h5>
             </div>
-            <IconsHeart :active="active" @click="active = !active" />
+            <div :class="$vuetify.display.smAndDown ? ' tw-rounded-full tw-bg-white tw-p-1 tw-absolute tw-left-1 tw-top-2 ' : ''" @click="active = !active">
+                <IconsHeart :active="active" />
+            </div>
         </div>
     </div>
 </template>
